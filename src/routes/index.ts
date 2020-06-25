5288107ceecfed4daea662a1a1f084a89f9e447b
amusameh <abdalsamad.y.m@gmail.com>
2020-06-25 16:41:39 +0300

    CHR-3: Update /hello-world endpoint to run a query that returns the current time from database

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import { QueryTypes } from 'sequelize';
+import { dbConfig } from '../database';
 
 const router = express.Router();
 
-router.get('/hello-world', (req, res) => {
-  res.send('Hello, World!');
+interface TimeNow {
+  now: Date;
+}
+
+router.get('/hello-world', async (req, res) => {
+  const [timeNow]: Array<TimeNow> = await dbConfig.query('SELECT NOW();', { type: QueryTypes.SELECT });
+  res.send(`Hello, World! ${timeNow.now.toLocaleString()}`);
 });
 
 export default router;
