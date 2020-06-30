0fd0ad29d8975b51ef0a77eef3025ac88ccc1ddc
amusameh <abdalsamad.y.m@gmail.com>
2020-06-30 11:46:02 +0300

    --amend

--- src/controllers/affiliateNetworks.ts
+++ src/controllers/affiliateNetworks.ts
@@ -3,6 +3,6 @@ import { NextFunction, Request, Response } from 'express';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getRakutenWebhookData = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   console.log('req.body', req.body);
-  console.log(req.headers);
-  res.status(200).send();
+  console.log('req.headers', req.headers);
+  res.status(200).send('ok');
 };
