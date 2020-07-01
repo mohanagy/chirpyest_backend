b1a68ef67bdb8fc450b7acc6246de21c128ff600
Mohammed Naji <moha.buy@gmail.com>
2020-07-01 15:06:38 +0300

    CHR-57: fix password validation

--- src/validations/auth.ts
+++ src/validations/auth.ts
@@ -1,8 +1,9 @@
 import joi from '@hapi/joi';
+import passwordComplexity from 'joi-password-complexity';
 
 export const signUpValidation = joi.object({
   email: joi.string().email().required(),
-  password: joi.string().min(8).required(),
+  password: passwordComplexity(),
   name: joi.string().required(),
   terms_conds_accepted: joi.boolean().valid(true).required(),
 });
