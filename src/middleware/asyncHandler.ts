307cb3b5e26c43b7be63cbb43a3c796c0529f0f1
Mohammed Naji <moha.buy@gmail.com>
2020-07-01 12:48:25 +0300

    CHR-57: disable eslint warning

--- src/middleware/asyncHandler.ts
+++ src/middleware/asyncHandler.ts
@@ -5,6 +5,8 @@ import { NextFunction, Request, Response } from 'express';
  * @param {function} fn the function or the controller which will be used for route
  * @return {*} result of executed function
  */
+
+// eslint-disable-next-line @typescript-eslint/ban-types
 export default (fn: Function) => async (request: Request, response: Response, next: NextFunction): Promise<void> => {
   try {
     return await fn(request, response, next);
