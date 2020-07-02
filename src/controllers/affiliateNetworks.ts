a6bec3596c5d2170ff7b3bf02aa728fbcc856ce1
amusameh <abdalsamad.y.m@gmail.com>
2020-07-02 07:27:41 +0300

    temp

--- src/controllers/affiliateNetworks.ts
+++ src/controllers/affiliateNetworks.ts
@@ -4,5 +4,9 @@ import { NextFunction, Request, Response } from 'express';
 export const getRakutenWebhookData = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   console.log('req.body', req.body);
   console.log('req.headers', req.headers);
+  // validate the data ??
+  // start transaction
+  // save to db
+  // update the pending money
   res.status(200).send('ok');
 };
