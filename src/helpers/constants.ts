a1cb518161b97b0c965c09b9217329b52ac2609c
amusameh <abdalsamad.y.m@gmail.com>
2020-07-05 15:18:33 +0300

    CHR-59: Extract error message to a constant

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -10,5 +10,6 @@ export const messages = {
   },
   general: {
     notFound: 'Not found',
+    commissionTypeError: 'TypeError: commission must be a number',
   },
 };
--- src/services/affiliateNetworks/utils.ts
+++ src/services/affiliateNetworks/utils.ts
@@ -1,6 +1,8 @@
+import { messages } from '../../helpers/constants';
+
 export const calculateCommission = (commission: number): number => {
   if (typeof commission !== 'number') {
-    throw new Error(`commission must be a number instead got ${commission} `);
+    throw new Error(messages.general.commissionTypeError);
   }
   const userCashBack = Math.round(commission / 2);
   return userCashBack;
