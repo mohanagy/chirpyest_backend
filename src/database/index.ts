64c07e60e1d76c909848b80ea0ad641e92e1f4da
Mohammed Naji <moha.buy@gmail.com>
2020-07-06 14:15:08 +0300

    CHR-41: disable database logging

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,7 +3,10 @@ import config from '../config';
 import { Database } from '../interfaces';
 import { UserFactory } from './models/users';
 
-export const dbConfig = new Sequelize(config.database.url, { define: { underscored: true, timestamps: true } });
+export const dbConfig = new Sequelize(config.database.url, {
+  define: { underscored: true, timestamps: true },
+  logging: false,
+});
 
 export const Users = UserFactory(dbConfig);
 
