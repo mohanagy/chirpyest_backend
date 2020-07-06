cc9a88c234898ca4e9096dd94ca4457e8bd0186d
Mohammed Naji <moha.buy@gmail.com>
2020-07-06 13:58:44 +0300

    CHR-41: implement tests cases for users profile and auth
    - fix auth test

--- src/__test__/routes/auth.ts
+++ src/__test__/routes/auth.ts
@@ -1,12 +1,24 @@
 import request from 'supertest';
 import app from '../../app';
+import { dto } from '../../helpers';
+import { removeCognitoUser } from '../../helpers/auth';
+import { usersServices } from '../../services';
 
 const email = `${Math.random().toString(36).substring(7)}@gmail.com`;
 describe('POST /api/v1/auth/signup endpoint', () => {
+  let cognitoId = '';
+  const filter = dto.generalDTO.filterData({ email });
+  after(async () => {
+    if (cognitoId) {
+      await usersServices.deleteUser(filter);
+      await removeCognitoUser(app, cognitoId);
+    }
+  });
+
   it('validation should fails the request since no data', (done) => {
     request(app).post('/api/v1/auth/signup').expect(400, done);
   });
-  it('validation should fails since password not match required rules', (done) => {
+  it('validation should fail because the password does not match required rules', (done) => {
     request(app)
       .post('/api/v1/auth/signup')
       .send({ email, password: '1234', termsCondsAccepted: true })
@@ -16,6 +28,14 @@ describe('POST /api/v1/auth/signup endpoint', () => {
     request(app)
       .post('/api/v1/auth/signup')
       .send({ email, password: '123asd!@#ASD', termsCondsAccepted: true })
+      .expect((res) => {
+        const {
+          body: {
+            data: { cognito_id: cognitoIdResult },
+          },
+        } = res;
+        cognitoId = cognitoIdResult;
+      })
       .expect(201, done);
   });
   it("should fails since can't create two accounts with the same email ", (done) => {
---
+++ src/__test__/routes/users.ts
@@ -0,0 +1,109 @@
+import { expect } from 'chai';
+import request from 'supertest';
+import app from '../../app';
+import { authHelpers, dto } from '../../helpers';
+import { usersServices } from '../../services';
+
+const email = `${Math.random().toString(36).substring(7)}@gmail.com`;
+describe('GET /api/v1/users/:id/profile endpoint', () => {
+  let cognitoId: string;
+  let userId: string;
+  let token: string;
+  const filter = dto.generalDTO.filterData({ email });
+  before(async () => {
+    const response = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({ email, password: '123asd!@#ASD', termsCondsAccepted: true });
+    const {
+      body: {
+        data: { cognitoId: cognitoIdResult, id },
+      },
+    } = response;
+    cognitoId = cognitoIdResult;
+    userId = id;
+    await authHelpers.confirmCognitoUser(app, email);
+    token = await authHelpers.authenticateUser(app, { Username: email, Password: '123asd!@#ASD' }, cognitoId);
+  });
+  after(async () => {
+    if (cognitoId) {
+      await usersServices.deleteUser(filter);
+      await authHelpers.removeCognitoUser(app, cognitoId);
+    }
+  });
+
+  it('every user can access his/her information only', () => {
+    request(app).get(`/api/v1/users/${userId}/profile`).set('Authorization', `Bearer ${token}`).expect(200);
+  });
+  it('every user should has list of data', (done) => {
+    request(app)
+      .get(`/api/v1/users/${userId}/profile`)
+      .set('Authorization', `Bearer ${token}`)
+      .end((error, response) => {
+        if (error) throw error;
+        expect(response.body.data).to.have.any.keys('name', 'image', 'id');
+        done();
+      });
+  });
+  it('user cannot access other profiles ', () => {
+    request(app).get(`/api/v1/users/9999/profile`).set('Authorization', `Bearer ${token}`).expect(401);
+  });
+});
+
+describe('PATCH /api/v1/users/:id/profile endpoint', () => {
+  let cognitoId: string;
+  let userId: string;
+  let token: string;
+  const filter = dto.generalDTO.filterData({ email });
+  const userData = {
+    name: 'testName',
+    image: 'https://picsum.photos/200',
+    paypalAccount: 'paypal@test.com',
+    newsletterSubscription: true,
+  };
+  before(async () => {
+    const response = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({ email, password: '123asd!@#ASD', termsCondsAccepted: true });
+    const {
+      body: {
+        data: { cognitoId: cognitoIdResult, id },
+      },
+    } = response;
+    cognitoId = cognitoIdResult;
+    userId = id;
+    await authHelpers.confirmCognitoUser(app, email);
+    token = await authHelpers.authenticateUser(app, { Username: email, Password: '123asd!@#ASD' }, cognitoId);
+  });
+  after(async () => {
+    if (cognitoId) {
+      await usersServices.deleteUser(filter);
+      await authHelpers.removeCognitoUser(app, cognitoId);
+    }
+  });
+
+  it('every user has the ability to edit his/her data', (done) => {
+    request(app)
+      .patch(`/api/v1/users/${userId}/profile`)
+      .set('Authorization', `Bearer ${token}`)
+      .set('Accept', 'application/json')
+      .send(userData)
+      .end((error, response) => {
+        if (error) throw error;
+        expect(response.body.data).to.eql({ id: userId, ...userData });
+        done();
+      });
+  });
+
+  it('user cannot update his/her email', (done) => {
+    request(app)
+      .patch(`/api/v1/users/${userId}/profile`)
+      .set('Authorization', `Bearer ${token}`)
+      .set('Accept', 'application/json')
+      .send({ email: 'test@test.com' })
+      .expect(400, done);
+  });
+
+  it('user cannot access other profiles', () => {
+    request(app).get(`/api/v1/users/9999/profile`).set('Authorization', `Bearer ${token}`).expect(401);
+  });
+});
