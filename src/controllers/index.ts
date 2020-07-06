f18db3c7b461726e9d6b6c0eefa594e13a016f65
Mohammed Naji <moha.buy@gmail.com>
2020-07-06 14:13:20 +0300

    CHR-41: implement user profile controller
    -  implement get user profile
    
    - implement update user profile controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,5 @@
 import * as auth from './auth';
+import * as users from './users';
 
 export const authControllers = auth;
+export const usersControllers = users;
---
+++ src/controllers/users.ts
@@ -0,0 +1,60 @@
+import { NextFunction, Request, Response } from 'express';
+import { Transaction } from 'sequelize/types';
+import { constants, dto, httpResponse } from '../helpers';
+import { usersServices } from '../services';
+
+/**
+ * @description signUp is a controller used to fetch user profile data
+ * @param {Request} request represents request object
+ * @param {Response} response represents response object
+ * @param {NextFunction} _next middleware function
+ * @return {Promise<Response>} object contains success status
+ */
+
+export const getUserProfile = async (
+  request: Request,
+  response: Response,
+  _next: NextFunction,
+  _transaction: Transaction,
+): Promise<Response> => {
+  const user = request.app.get('user');
+  const userId = dto.usersDTO.userId(request);
+  if (!user || Number(user.id) !== Number(userId.id)) {
+    return httpResponse.unAuthorized(response, constants.messages.auth.notAuthorized);
+  }
+  return httpResponse.ok(response, dto.usersDTO.userProfileResponse(user), constants.messages.users.userProfile);
+};
+/**
+ * @description updateUserProfile is a controller used to update user profile data
+ * @param {Request} request represents request object
+ * @param {Response} response represents response object
+ * @param {NextFunction} _next middleware function
+ * @return {Promise<Response>} object contains success status
+ * @returns Promise
+ */
+export const updateUserProfile = async (
+  request: Request,
+  response: Response,
+  _next: NextFunction,
+  transaction: Transaction,
+): Promise<Response> => {
+  const user = request.app.get('user');
+  const userId = dto.usersDTO.userId(request);
+  const bodyData = dto.generalDTO.bodyData(request);
+  const userData = dto.usersDTO.userProfileUpdatableFields(bodyData);
+
+  if (!user || Number(user.id) !== Number(userId.id)) {
+    return httpResponse.unAuthorized(response, constants.messages.auth.notAuthorized);
+  }
+
+  const filter = dto.generalDTO.filterData(userId);
+
+  const [, [updateUserDataResult]] = await usersServices.updateUser(filter, userData, transaction);
+
+  transaction.commit();
+  return httpResponse.ok(
+    response,
+    dto.usersDTO.userProfileResponse(updateUserDataResult),
+    constants.messages.users.updateUserProfileSuccess,
+  );
+};
