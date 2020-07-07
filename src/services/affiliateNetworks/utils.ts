80009df54b43fdff3986c222a8b5538346681777
amusameh <abdalsamad.y.m@gmail.com>
2020-07-07 09:50:27 +0300

    CHR-59: Add function description

--- src/services/affiliateNetworks/utils.ts
+++ src/services/affiliateNetworks/utils.ts
@@ -1,5 +1,11 @@
 import { messages } from '../../helpers/constants';
 
+/**
+ * @description calculate the user cash back amount
+ * @param commission the commission given to chirpyest
+ * @return Number
+ */
+
 export const calculateCommission = (commission: number): number => {
   if (typeof commission !== 'number') {
     throw new Error(messages.general.commissionTypeError);
