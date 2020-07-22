d1d980b6fa69f6b49f3c6316727408172b231cbf
Mohammed Naji <moha.buy@gmail.com>
2020-07-22 12:37:15 +0300

    CHR-96 : fix typo

--- src/services/affiliateNetworks/index.ts
+++ src/services/affiliateNetworks/index.ts
@@ -1,5 +1,5 @@
 import * as CommissionJunction from './commissionJunction';
-import * as Rakuten from './Rakuten';
+import * as Rakuten from './rakuten';
 
 export const rakutenServices = Rakuten;
 export const commissionJunctionServices = CommissionJunction;
