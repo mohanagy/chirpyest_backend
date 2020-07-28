3b2960a070656453da5a566b79f120913ee4e915
Mohammed Naji <moha.buy@gmail.com>
2020-07-28 11:51:56 +0300

    add try and catch for migration call

--- migration.js
+++ migration.js
@@ -32,5 +32,9 @@ const umzug = new Umzug({
   // Checks migrations and run them if they are not already applied. To keep
   // track of the executed migrations, a table (and sequelize model) called SequelizeMeta
   // will be automatically created (if it doesn't exist already) and parsed.
-  await umzug.up();
+  try {
+    await umzug.up();
+  } catch (error) {
+    console.error(error);
+  }
 })();
