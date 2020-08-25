eb82efa743bbaf01ea92229894bc8ef76217803f
Mohammed Naji <moha.buy@gmail.com>
2020-08-25 13:00:59 +0300

    CHR-176: modify verifyTtoken call function

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,15 +1,19 @@
 import express from 'express';
+import { UserTypes } from '../interfaces';
 import { usersControllers } from '../controllers';
 import { asyncHandler, validate, verifyToken } from '../middleware';
 import { usersValidation } from '../validations';
 
 const router = express.Router();
 
-router.get('/users/:id/profile', asyncHandler(verifyToken), asyncHandler(usersControllers.getUserProfile));
-router.get('/users', asyncHandler(usersControllers.getUsersList));
+router.get(
+  '/users/:id/profile',
+  asyncHandler(verifyToken([UserTypes.Customer])),
+  asyncHandler(usersControllers.getUserProfile),
+);
 router.patch(
   '/users/:id/profile',
-  asyncHandler(verifyToken),
+  asyncHandler(verifyToken([UserTypes.Customer])),
   validate.body(usersValidation.updateUserProfile),
   asyncHandler(usersControllers.updateUserProfile),
 );
