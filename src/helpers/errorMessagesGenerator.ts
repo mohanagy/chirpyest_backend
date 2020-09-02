9d958dfce0495aea14ee6acbd4e102da56c21142
Mohammed Naji <moha.buy@gmail.com>
2020-09-02 20:51:56 +0300

    CHR-140: fix the error message and update the bucket

--- src/helpers/errorMessagesGenerator.ts
+++ src/helpers/errorMessagesGenerator.ts
@@ -1,5 +1,10 @@
 import { ErrnoException } from '../interfaces';
 
 export default (error: ErrnoException): string => {
-  return (error?.errors && error.errors[0]?.message) || error.message || error.error?.details || 'Internal server';
+  return (
+    (error?.errors && error.errors[0]?.message) ||
+    error.message ||
+    error.error?.details[0]?.message ||
+    'Internal server'
+  );
 };
--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -7,7 +7,8 @@ import { S3StreamLogger } from 's3-streamlogger';
 import config from '../config';
 
 const s3stream = new S3StreamLogger({
-  bucket: 'chirpyest-dev-logs',
+  bucket: config.cognito.awsConfigs.bucketName,
+  folder: 'logs',
   access_key_id: config.cognito.awsConfigs.awsS3AccessKeyId,
   secret_access_key: config.cognito.awsConfigs.awsS3SecretAccessKey,
   config: {
