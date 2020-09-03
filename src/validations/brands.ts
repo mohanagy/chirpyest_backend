3e2a223321cc093e3a42ffc59118fa6adf869568
Mohammed Naji <moha.buy@gmail.com>
2020-09-03 21:39:17 +0300

    CHR-195: fix regex

--- src/validations/brands.ts
+++ src/validations/brands.ts
@@ -7,7 +7,7 @@ export const brandsQuery = joi.object({
   category: joi.valid(...categories),
 });
 
-const urlPattern = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+const urlPattern = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
 export const shortLinksValidate = joi.object({
   url: joi
     .string()
